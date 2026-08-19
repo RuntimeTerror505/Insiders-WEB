a48d4883436e252af0b3c6150da9c4d57f9548e6
RuntimeTerror505 <andriyilkiv322@gmail.com>
2026-08-19 17:26:59 +0300

    project and configuration initialization

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Insiders</h1>
+
+            <Link href="/">
+            </Link>
+        </header>
+    );
+}
---
+++ src/lib/schema.ts
@@ -0,0 +1,1 @@
+// zod: frontmatter + блоки
